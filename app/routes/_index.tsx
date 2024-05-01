410d75d3b0c1e5341c319a1a068588991016c09f
Dorian Neto <doriansampaioneto@gmail.com>
2024-05-01 17:51:08 -0230

    feat: install chakra ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,14 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+import { Alert, AlertDescription, AlertIcon, AlertTitle, CircularProgress } from "@chakra-ui/react";
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <Alert status='error'>
+        <AlertIcon />
+        <AlertTitle>Your browser is outdated!</AlertTitle>
+        <AlertDescription>Your Chakra experience may be degraded.</AlertDescription>
+      </Alert>
+      <CircularProgress isIndeterminate color='green.300' />
     </div>
   );
 }
